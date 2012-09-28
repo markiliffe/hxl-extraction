0b7b9fa6aaa7ad1945f0bbc4be3d024d38e501a5
Phil Turnbull <philip.turnbull@googlemail.com>
2012-09-28 16:00:04 +0100

    Add APL class.

--- hxl/__init__.py
+++ hxl/__init__.py
@@ -1,2 +1,9 @@
 class HXLException(Exception):
 	pass
+
+class APL(object):
+       def __init__(self, pcode, featureName, point):
+               self.pcode = pcode
+               self.featureName = featureName
+               self.point = point
+
